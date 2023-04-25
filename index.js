3da670e59f27b7b33d464026cf813a8643e3dda9
Jeremy Garcia <jeremygarcia9400@gmail.com>
2023-04-25 14:14:10 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,4 +1,59 @@
-/* Your Code Here */
+function createEmployeeRecord(employee) {
+    const employeeRecord = {
+        'firstName': employee[0],
+        'familyName': employee[1],
+        'title': employee[2],
+        'payPerHour': employee[3],
+        'timeInEvents': [],
+        'timeOutEvents': []
+    }
+    return employeeRecord;
+}
+function createEmployeeRecords(employees) {
+    return employees.map(createEmployeeRecord);
+}
+function createTimeInEvent(dateTimeStamp) {
+    if (dateTimeStamp.length === 15 && dateTimeStamp[10] === ' ') {
+        this.timeInEvents = [...this.timeInEvents, {
+            'type': 'TimeIn',
+            'date': dateTimeStamp.split(' ')[0],
+            'hour': Number(dateTimeStamp.split(' ')[1])
+        }]
+        return this;
+    } else {
+        alert('Wrong date/time format!')
+    }
+}
+function createTimeOutEvent(dateTimeStamp) {
+    if (dateTimeStamp.length === 15 && dateTimeStamp[10] === ' ') {
+        this.timeOutEvents = [...this.timeOutEvents, {
+            'type': 'TimeOut',
+            'date': dateTimeStamp.split(' ')[0],
+            'hour': Number(dateTimeStamp.split(' ')[1])
+        }]
+        return this;
+    } else {
+        alert('Wrong date/time format!')
+    }
+}
+function hoursWorkedOnDate(dateStamp) {
+    let timeIn, timeOut;
+    const timeInElements = this.timeInEvents;
+    const timeOutElements = this.timeOutEvents;
+    for (const timeInElement of timeInElements) {
+        for (const timeOutElement of timeOutElements) {
+            if (timeInElement.date === dateStamp && timeOutElement.date === dateStamp) {
+                timeIn = timeInElement.hour;
+                timeOut = timeOutElement.hour;
+            }
+        }
+    }
+    return (timeOut - timeIn)/100;
+}
+function wagesEarnedOnDate(dateStamp) {
+    const hours = hoursWorkedOnDate.call(this, dateStamp);
+    return hours * this.payPerHour;
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -21,3 +76,13 @@ const allWagesFor = function () {
     return payable
 }
 
+function findEmployeeByFirstName(srcArray, firstName) {
+    return srcArray.find(function(employee) {return employee.firstName === firstName});
+}
+function calculatePayroll(employees) {
+    let sum = 0;
+    for (const employee of employees) {
+        sum += allWagesFor.call(employee);
+    }
+    return sum;
+}
